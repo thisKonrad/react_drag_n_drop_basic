34b39fc6929688440b3b7d4965fea4d02e777caf
this.Konrad <konradlebt@gmail.com>
2024-02-20 15:25:55 +0100

    get text input

--- components/Notes/index.jsx
+++ components/Notes/index.jsx
@@ -2,6 +2,14 @@
 import styles from './style.module.css';
 
 export default function Notes() {
+  function inputValue(event) {
+    const text = event.target.value;
+    console.log(text);
+  }
+  function addItem(event) {
+    event.preventDefault();
+    console.log('click');
+  }
   return (
     <div className={styles.wrap}>
       <p className={styles.heading}>drag n drop kit react</p>
@@ -10,6 +18,7 @@ export default function Notes() {
           note:
           <input
             className={styles.note}
+            onChange={inputValue}
             type="text"
             name="addTopping"
             min="4"
@@ -17,6 +26,7 @@ export default function Notes() {
             autoComplete="off"
           />
         </label>
+        <button onClick={addItem}>add</button>
       </form>
     </div>
   );
