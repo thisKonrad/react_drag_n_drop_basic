43cf7fcd451de04dc088de99065c3106e2b6ecd5
this.Konrad <konradlebt@gmail.com>
2024-02-21 14:01:10 +0100

    drag n drop with react no liabries

--- components/DropZone/index.jsx
+++ components/DropZone/index.jsx
@@ -3,19 +3,39 @@ import { useState, useRef } from 'react';
 
 import styles from './style.module.css';
 
-export default function DropZone({ player, id }) {
+export default function DropZone() {
+  const [player, setPlayer] = useState([
+    { id: 1, title: 'T-Rex', emoji: '🦖' },
+    { id: 2, title: 'unicorn', emoji: '🦄' },
+    { id: 3, title: 'robot', emoji: '🤖' },
+    { id: 4, title: 'dracula', emoji: '🧛' },
+    { id: 5, title: 'picadilly', emoji: '🧚' },
+    { id: 6, title: 'stonehead', emoji: '🗿' },
+  ]);
+
   const dragPlayer = useRef(0);
   const dragOverPlayer = useRef(0);
+
+  function handleDragSort() {
+    const copyPlayer = [...player];
+    const actualPlayer = copyPlayer[dragPlayer.current];
+    copyPlayer[dragPlayer.current] = copyPlayer[dragOverPlayer.current];
+    copyPlayer[dragOverPlayer.current] = actualPlayer;
+    setPlayer(copyPlayer);
+  }
+
   return (
     <section className={styles.wrap}>
-      <p className={styles.title}>hello player!</p>
+      <p className={styles.title}>hello draggers!</p>
       {player &&
-        player.map((item) => (
+        player.map((item, index) => (
           <div
             key={item.id}
             draggable
-            onDragStart={() => (dragPlayer.current = item.id)}
-            onDragEnter={() => (dragOverPlayer.current = item.id)}
+            onDragStart={() => (dragPlayer.current = index)}
+            onDragEnter={() => (dragOverPlayer.current = index)}
+            onDragEnd={handleDragSort}
+            onDragOver={(event) => event.preventDefault()}
           >
             <p className={styles.player}>
               {item.emoji}:{item.title}
--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,11 @@
 /* :::: APP :::: */
-import { useState } from 'react';
 import DropZone from '../components/DropZone/index.jsx';
 import './App.css';
 
 function App() {
-  const [player, setPlayer] = useState([
-    { id: 1, title: 'T-Rex', emoji: '🦖' },
-    { id: 2, title: 'unicorn', emoji: '🦄' },
-    { id: 3, title: 'robot', emoji: '🤖' },
-    { id: 4, title: 'dracula', emoji: '🧛' },
-    { id: 5, title: 'picadilly', emoji: '🧚' },
-    { id: 6, title: 'stonehead', emoji: '🗿' },
-  ]);
-
   return (
     <div>
-      <DropZone player={player} id={player.id} />
+      <DropZone />
     </div>
   );
 }
